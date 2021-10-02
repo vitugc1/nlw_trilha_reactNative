7c66ff0e7d70f77967aa305d616755df1e9a41f5
victorgc <victorcarvalho13022002@gmail.com>
2021-10-02 10:38:37 -0300

    primeiro commit

---
+++ src/screen/Signin/index.tsx
@@ -0,0 +1,44 @@
+import React,{useState} from 'react';
+import { Text, View, Image, StatusBar } from 'react-native';
+
+import  IllustrationImg  from '../../assets/illustration.png'
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { styles } from './styles'
+
+export function Signin() {
+    const [text, setText] = useState('');
+
+    return (
+        <View style={styles.container}>
+            <StatusBar 
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+            />
+            <Image 
+            source={ IllustrationImg } 
+            style={styles.image} 
+            resizeMode="stretch"
+            />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas{`\n`}
+                    facilmente
+                </Text>
+                <Text style={styles.subTitle}>
+                    Crie grupos para jogar seus games{`\n`}
+                    favoritos com seus amigos
+                </Text>
+
+                <ButtonIcon 
+                title="Entrar com Discord"
+                activeOpacity={0.7}
+                 />
+
+            </View>
+        </View>
+  )
+}
+
